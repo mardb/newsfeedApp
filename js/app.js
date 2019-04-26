dceff4a6e21c685748065c1a2cd5b5f567458892
Marlene Guzman <marleneguzmanla@gmail.com>
2019-04-25 19:59:51 -0700

    completion

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 const $odd = $('a:odd');
-const $secureLinks = $('a[href^ = "https://"]');
-const $pdfs = $('a[href$ = ".pdf"]');
-const $pdfCheckbox = $(
-  ' <label><input type="checkbox" /> Allow PDF downloads</label>'
-);
+const $secureLinks = $('a[href^="https://"]');
+const $pdfs = $('a[href$=".pdf"]');
+const $pdfCheckbox = $('<label><input type="checkbox"> Allow PDF downloads</label>');
 
 $secureLinks.attr('target', '_blank');
 $pdfs.attr('download', true);
@@ -12,15 +10,21 @@ $secureLinks.addClass('secure');
 $pdfs.addClass('pdf');
 
 $pdfs.on('click', function(event){
-  
 //check if the checkbox is checked
 //if not checked
-if($(':checked').length === 0 ){
-//prevent default
-event.preventDefault();
-//alert user
-alert("Please check the box to allow PDF downloads.")
-} 
+if ($(":checked").length === 0) {
+  //prevent download
+  event.preventDefault();
+  //alert user
+  alert("Please check the box to allow PDF downloads.");
+  } 
 });
 
-$('#links').append($pdfCheckbox)
+$('#links').append($pdfCheckbox);
+
+$('a').each(function(){
+  const url = $(this).attr('href');
+  $(this).parent().append(`(${url})`);
+
+});
+
