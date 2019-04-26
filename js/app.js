84cbe76d001181ef833ff22f5132e17ac194c7cd
Marlene Guzman <marleneguzmanla@gmail.com>
2019-04-25 19:10:24 -0700

    prevent browser default

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
 const $odd = $('a:odd');
 const $secureLinks = $('a[href^ = "https://"]');
 const $pdfs = $('a[href$ = ".pdf"]');
+const $pdfCheckbox = $(
+  ' <label><input type="checkbox" /> Allow PDF downloads</label>'
+);
 
 $secureLinks.attr('target', '_blank');
-
 $pdfs.attr('download', true);
+
+$secureLinks.addClass('secure');
+$pdfs.addClass('pdf');
+
+$pdfs.on('click', function(event){
+  
+//check if the checkbox is checked
+//if not checked
+if($(':checked').length === 0 ){
+//prevent default
+event.preventDefault();
+//alert user
+alert("Please check the box to allow PDF downloads.")
+} 
+});
+
+$('#links').append($pdfCheckbox)
